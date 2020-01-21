1099f1e4fc0620f202f03ed7296a9c196d9b6d81
Essam Salah <essam@hashfood.com>
2020-01-21 10:18:31 +0300

    first commit

---
+++ backend/hello.py
@@ -0,0 +1,12 @@
+from time import time, ctime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/time')
+def show_time():
+    return ctime(time())
